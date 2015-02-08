a5365e417e7de8f033c94ff0fbdd22cfab22e0f3
Daniel Stern <daniel@danielstern.ca>
2015-02-08 13:25:05 -0500

    prob chart oin god

--- app/scripts/allColorsGrouped.js
+++ app/scripts/allColorsGrouped.js
@@ -1,3 +1,8 @@
+function reg(a){
+  return a.group !== 'Utilities' &&
+    a.group !== 'Special' &&
+    a.group !== 'Railroad';
+}
 (function() {
   var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
@@ -29,11 +34,7 @@
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  function reg(a){
-    return a.group !== 'Utilities' &&
-      a.group !== 'Special' &&
-      a.group !== 'Railroad';
-  }
+
   //d3.csv("data.csv", function(error, data) {
   d3.json("monopoly.json", function(error, res) {
     var properties = res.properties.reduce(function(a,b){
@@ -66,26 +67,26 @@
         "Three houses":property.price + property.housecost * 3,
         "Four houses":property.price + property.housecost * 4,
         "Hotel":property.price + property.housecost * 5,
-        State:property.group,
+        group:property.group,
       }
     })
   //  debugger;
 
     console.log(data);
-    var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
+    var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "group"; });
 
     data.forEach(function(d) {
-      d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+      d.ages = ageNames.map(function(name) { return {name: name, value: +d[name],group: d.group}; });
     });
 
-    x0.domain(data.map(function(d) { return d.State; }));
+    x0.domain(data.map(function(d) { return d.group; }));
     x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
     y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    //svg.append("g")
+    //  .attr("class", "x axis")
+    //  .attr("transform", "translate(0," + height + ")")
+    //  .call(xAxis);
 
     svg.append("g")
       .attr("class", "y axis")
@@ -95,22 +96,22 @@
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Population");
+      .text("Cost to Build Up");
 
-    var state = svg.selectAll(".state")
+    var group = svg.selectAll(".group")
       .data(data)
       .enter().append("g")
       .attr("class", "g")
-      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
+      .attr("transform", function(d) { return "translate(" + x0(d.group) + ",0)"; });
 
-    state.selectAll("rect")
+    group.selectAll("rect")
       .data(function(d) { return d.ages; })
       .enter().append("rect")
       .attr("width", x1.rangeBand())
       .attr("x", function(d) { return x1(d.name); })
       .attr("y", function(d) { return y(d.value); })
       .attr("height", function(d) { return height - y(d.value); })
-      .style("fill", function(d) { return color(d.name); });
+      .style("fill", function(d,i) { return d3.hsl(d.group).brighter(i/10); });
 
     var legend = svg.selectAll(".legend")
       .data(ageNames.slice().reverse())
@@ -118,18 +119,18 @@
       .attr("class", "legend")
       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-    legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+    //legend.append("rect")
+    //  .attr("x", width - 18)
+    //  .attr("width", 18)
+    //  .attr("height", 18)
+    //  .style("fill", color);
 
-    legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
+    //legend.append("text")
+    //  .attr("x", width - 24)
+    //  .attr("y", 9)
+    //  .attr("dy", ".35em")
+    //  .style("text-anchor", "end")
+    //  .text(function(d,i) { return i + " " + d; });
 
   });
 })();
---
+++ app/scripts/probabilities.js
@@ -0,0 +1,83 @@
+(function() {
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+  var y = d3.scale.linear()
+    .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
+
+  var svg = d3.select("#probabilities").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.json("monopoly.json", function (error, res) {
+
+    //debugger;
+    //var data = res.properties.filter(reg);
+    var data = res.properties;
+    x.domain(data.map(function (d) {
+      d.averageProbability = d.averageProbability / 100;
+      return d.name;
+    }));
+    y.domain([0, d3.max(data, function (d) {
+      return d.averageProbability;
+    })]);
+    //debugger;
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("averageProbability");
+
+    svg.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr('fill', function (d) {
+        if (d.group === 'Utilities') return 'lightgray';
+        if (d.group === 'Railroad') return 'darkgray';
+        if (d.group === 'Special') return 'brown';
+        return d.group
+      })
+      .attr("x", function (d) {
+        return x(d.name);
+      })
+      .attr("width", x.rangeBand())
+      .attr("y", function (d) {
+        return y(d.averageProbability);
+      })
+      .attr("height", function (d) {
+        return height - y(d.averageProbability);
+      });
+
+  });
+
+  function type(d) {
+    d.averageProbability = +d.averageProbability;
+    return d;
+  }
+})();
